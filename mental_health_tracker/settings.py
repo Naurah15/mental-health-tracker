fbbdcc413fb89a760f72971ffcaeb2c7d0c6cef2
Naurah15 <naunaurahiradya@gmail.com>
2024-08-28 14:45:08 +0700

    Web PBP

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o8j#e6rxsmji_ziiste-vfnizww09)*6j^=nufu0#habo)*z49
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "naurah-iradya-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 
 # Application definition
